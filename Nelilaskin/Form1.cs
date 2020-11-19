c2687f260c6e04a64a320675b77228d1e56e34b8
Jokemon-coder <Jokemon-coder@users.noreply.github.com>
2020-11-19 12:05:03 +0200

    Edit

--- Nelilaskin/Form1.cs
+++ Nelilaskin/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Nelilaskin
+namespace Nelilaskin //koodi alkaa
 {
     public partial class Nelilaskin : Form
     {
@@ -20,12 +20,12 @@ namespace Nelilaskin
             InitializeComponent();
         }
 
-        private void poistu_Click(object sender, EventArgs e)
+        private void poistu_Click(object sender, EventArgs e) 
         {
-            Application.Exit();
+            Application.Exit(); //Nappi jolla pääsee poistumaan ohjelmasta
         }
 
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) //About-osio, josta näkee tekijän ja ohjeet Copy-painikkeen käyttöön
         {
             MessageBox.Show("Tekijä: Joel\nCopy-painikkeen käyttö:\nValitse luku, valitse laskutapa,\npaina 'Copy' ja paina yhtäsuuruusmerkkiä.", "Nelilaskin About");
         }
@@ -39,19 +39,19 @@ namespace Nelilaskin
 
         private void nappiCopy_Click(object sender, EventArgs e)
         {
-            Clipboard.SetText(tulos.Text);
+            Clipboard.SetText(tulos.Text); //Ottaa syöttökentästä siinä olevan luvun ja kopioi sen
         }
 
-        private void nappiReset_Click(object sender, EventArgs e)
+        private void nappiReset_Click(object sender, EventArgs e) 
         {
-            tulos.Clear();
+            tulos.Clear(); //Puhdistaa koko syöttökentän
         }
 
-        private void nappiClear_Click(object sender, EventArgs e)
+        private void nappiClear_Click(object sender, EventArgs e) //Saa poistettua numerot yksitellen syöttökentästä
         {
-            if (tulos.Text.Length > 0)
+            if (tulos.Text.Length > 0) //Jos syöttökentässä olevat "teksti" on enemmän kuin yhden hahmon, 
             {
-                tulos.Text = tulos.Text.Remove(tulos.Text.Length - 1, 1);
+                tulos.Text = tulos.Text.Remove(tulos.Text.Length - 1, 1); //Otetaan "tekstin" perästä yksi hahmo pois
             }
             
         }
@@ -59,10 +59,10 @@ namespace Nelilaskin
 
         private void NappiNum_Click(object sender, EventArgs e)
         {
-            if ((tulos.Text == "0") || (operaatio_Click))
+            if ((tulos.Text == "0") || (operaatio_Click)) //Ei voi laittaa alusta enemmän kuin yksi nolla ensimmäisen nollan perään
                 tulos.Clear();
-            operaatio_Click = false;
-            Button b = (Button)sender;
+            operaatio_Click = false; //bool operaatio_Click pistetään falseksi
+            Button b = (Button)sender; //Muutetaan nappi lähettäjäksi
             tulos.Text = tulos.Text + b.Text;
         }
 
@@ -73,17 +73,17 @@ namespace Nelilaskin
 
         private void nappiOperaattori_Click(object sender, EventArgs e)
         {
-            Button b = (Button)sender;
-            operaatio = b.Text;
-            arvo = Double.Parse(tulos.Text);
-            operaatio_Click = true;
+            Button b = (Button)sender; //Pistetään nappi lähettäjäksi
+            operaatio = b.Text; //string operaatio pistetään samaksi kuin b.Text
+            arvo = Double.Parse(tulos.Text); //arvo muutetaan luvuiksi
+            operaatio_Click = true; //operaatio_Click laitetaan takaisin trueksi
             
         }
 
         private void nappiEqual_Click(object sender, EventArgs e) //Operaatiot otetaan mukaan laskuun ja yhteenlasku-painike laskee laskun ja antaa tuloksen
         {
 
-            switch (operaatio)
+            switch (operaatio) //Switch loop laskupainikkeille, jottei niille tarvitse jokaiselle tehdä erillistä metodia
             {
                 case "+":
                     tulos.Text = (arvo + Double.Parse(tulos.Text)).ToString();
@@ -99,7 +99,7 @@ namespace Nelilaskin
                     break;
                 default:
                     break;
-            }//lopeta switch
+            } //lopeta switch
         }
     }
-}
+} //koodi päättyy
